f3f08499e9252573bb93098904fcc8cf1713fe07
Muhtar <mqnn12@yahoo.com>
2021-06-04 21:42:42 -0400

    Add project files.

---
+++ BlazorWASMDependencyInjection/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorWASMDependencyInjection.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWASMDependencyInjection.Services
+{
+    public interface IUserService
+    {
+        public Task<IEnumerable<User>> GetUsers();
+    }
+}
---
+++ BlazorWASMDependencyInjection/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System.Net.Http.Json;
+using BlazorWASMDependencyInjection.Models;
+
+namespace BlazorWASMDependencyInjection.Services
+{
+    public class UserService: IUserService
+    {
+        private static string jsonPlaceHolderApiUrl = "https://jsonplaceholder.typicode.com/users";
+
+        private readonly HttpClient httpClient;
+
+        public UserService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            return await this.httpClient.GetFromJsonAsync<IEnumerable<User>>(jsonPlaceHolderApiUrl);
+        }
+
+    }
+}
